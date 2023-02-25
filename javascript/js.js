41bf7cbc43a18fbdf7aa91c67998ff42550312fa
Leandrocabralsantana <dburgos221@gmail.com>
2023-02-25 16:33:50 -0300

    Commit

--- javascript/js.js
+++ javascript/js.js
@@ -1,11 +1,12 @@
 //funcion que me aplica el estilo a la opcion seleccionada en el menu y quita la previamente seleccionada
 function seleccionar(link){
-    var opciones = document.querySelector ('#links a')
+    var opciones = document.querySelectorAll ('#links a');
     opciones [0].className = "";
     opciones [1].className = "";
     opciones [2].className = "";
     opciones [3].className = "";
     opciones [4].className = "";
+    link.className = "seleccionado";
 //desaparece el menu una vez que se ha seleccionado la opcion responsive 
 var  x = document.getElementById ("nav");
 x.className = "";
@@ -19,4 +20,20 @@ function responsiveMenu(){
     } else {
         x.className = "";
     }
+}
+//para animar las barritas de habilidadesss
+window.onscroll = function(){ 
+    efectoHabilidades() 
+};
+//para aplicar animacion en las barritas
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+
 }
